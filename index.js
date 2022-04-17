14357f9efd2a1f60ed7cc62dd5617552c4468325
Henry Díaz <61124185+h3nry-d1az@users.noreply.github.com>
2022-04-17 15:57:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fetch = require('node-fetch');
 
 let fetchcmd = async (message, args, client, _, __) => {
-  const url = args.join("/")
+  const url = args[0]
   if(!url) return message.reply("`Give me an URL to fetch`");
 
   let res = await fetch(url);
   let data = await res.text()
 
   if (data.length === 0) return message.reply(`\`Couldn't fetch ${url}\``);
-  if (data.length > 4000) return message.reply(`\`\`\`${data.slice(0, 3996).concat('...')}\`\`\``):
+  if (data.length > 1000) return message.reply(`\`\`\`${data.slice(0, 996).concat('...')}\`\`\``);
   message.reply(`\`\`\`${data}\`\`\``);
 }
 
